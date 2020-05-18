478f2ca2197a63eb450ef14f883a4c3e672c54b2
kokot300 <kokot300@gmail.com>
2020-05-18 20:53:54 +0200

    working on code refractorization

--- dice_main.py
+++ dice_main.py
@@ -4,25 +4,31 @@ import dices_lib as dl
 
 
 def main():
+    """
+    main function for program. function is practically an infinite loop that makes user introduce kind of throw
+    and displays points scored.
+    :return: None
+    """
     print('Format: xDy+z \nx - how many throws y - size of the dice z - what to sum to the result')
     while True:
         kind_of_throw = input('tell me what do you want to throw: \n')
-        standarized_throw = dl.throw_standardizer(kind_of_throw)
-        # print(standarized_throw[0])
-        # print(standarized_throw[1])
-        # print(standarized_throw[2])
-        try:
-            int(standarized_throw[0])
-            int(standarized_throw[1])
-            int(standarized_throw[2])
-            print('input correct')
-        except:
-            print('you messed up the input. please try again')
-            continue
-        if len(standarized_throw) != 3:
-            print('something is rotten in the state of Denmark')
-            continue
-        print(dl.lets_throw(standarized_throw))
+
+
+        standarized_throw = dl.throw_validator(kind_of_throw)
+        print(standarized_throw)
+
+        # try:
+        #     int(standarized_throw[0])
+        #     int(standarized_throw[1])
+        #     int(standarized_throw[2])
+        #     print('input correct')
+        # except TypeError:
+        #     print('you messed up the input. please try again')
+        #     continue
+        # if len(standarized_throw) != 3:
+        #     print('something is rotten in the state of Denmark')
+        #     continue
+        # print(dl.lets_throw(standarized_throw))
 
 
 if __name__ == '__main__':
--- dices_lib.py
+++ dices_lib.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from random import randint
+from re import split, findall
 
 
 def dice_throw(kind_of_dice):
@@ -18,120 +19,35 @@ def lets_throw(throw_tulpe):
     return result
 
 
-def find_x(usr_str):
-    x = 0
-    for i in usr_str:
-        if i == 'd':
-            break
-        elif i == 'D':
-            break
-        else:
-            try:
-                x += int(i)
-                return x
-            except:
-                return 'unable to find x'
+def throw_validator(user_wish='D66+-2'):
+    throws_number = split("[d|D]", user_wish)[0]
+    if throws_number == '':
+        throws_number = '1'
+    print('throws_number ', throws_number)
 
+    kind_of_dice = split("[d|D]|[+|-]", user_wish)[1]
+    if kind_of_dice == '':
+        kind_of_dice = '6'
+    print('kind_of_dice ', kind_of_dice)
 
-def find_y(usr_str):
-    y = 0
+    if findall("[+-]|[-+]", user_wish) == []:
+        return print('some')
+    elif findall("[+|-]", user_wish) == []:
+        user_wish = user_wish + '+0'
+    final_add = split("[+|-]", user_wish)[1]
+    if final_add == '':
+        final_add = '0'
+    if findall('[-]', user_wish):
+        final_add = '-' + final_add
+    print(final_add)
 
-    for i in range(len(usr_str)):
-        if usr_str[i] == 'd' or usr_str[i] == 'D':
-            starting_point = i + 1
-            break
-        else:
-            starting_point = 0
-
-    if starting_point == 0:  # might be referenced before assignment, but no idea how
-        return 'unable to find y'
-    short_str = usr_str[starting_point:]
-    for i in short_str:
-        try:
-            y += int(i)
-        except:
-            # y = 'unable to find y'
-            break
-    if y == 0:
-        y = 'unable to find y'
-    return y
-
-
-def find_z(usr_str):
-    if len(usr_str.split('+')) == 2:
-        lst = usr_str.split('+')
-    elif len(usr_str.split('-')) == 2:
-        lst = usr_str.split('-')
-        usr_str = str(lst[0] + '+-' + lst[1])
-        lst = usr_str.split('+')
-    else:
-        usr_str = usr_str + '+0'
-        lst = usr_str.split('+')
-
-    z = lst[1]
     try:
-        z = int(z)
-        return z
-    except:
-        return 'format for z is incorrect!'
+        int(throws_number)
+        int(kind_of_dice)
+        int(final_add)
+    except ValueError:
+        print('input incorrect')
 
 
-def input_checker(usr_str):
-    ok_flag = True
-    allowed_chars = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'd', 'D', '+', '-']
-    for i in usr_str:
-        for ii in allowed_chars:
-            if i == ii:
-                ok_flag = True
-                break
-            else:
-                ok_flag = False
-        if ok_flag == False:
-            return ok_flag
-    return ok_flag
-
-
-def throw_standardizer(usr_str):
-    ok_flag = input_checker(usr_str)
-    if ok_flag == False:
-        return 'input incorrect', 'input incorrect', 'input incorrect'
-    x = find_x(usr_str)
-    y = find_y(usr_str)
-    z = find_z(usr_str)
-    return x, y, z
-    # if len(usr_str.split('+')) == 2:
-    #     lst = usr_str.split('+')
-    # elif len(usr_str.split('-')) == 2:
-    #     lst = usr_str.split('-')
-    #     usr_str = str(lst[0] + '+-' + lst[1])
-    #     lst = usr_str.split('+')
-    # else:
-    #     usr_str = usr_str + '+0'
-    #     lst = usr_str.split('+')
-    #
-    # z=lst[1]
-    # try:
-    #     z=int(z)
-    # except:
-    #     exit('format for z is incorrect!')
-    #
-    # xy=lst[0]
-    # if len(xy.split('d')) == 2:
-    #     xy=xy.split('d')
-    #     x=xy[0]
-    #     y=xy[1]
-    # elif len(xy.split('D')) == 2:
-    #     xy = xy.split('D')
-    #     x = xy[0]
-    #     y = xy[1]
-    # else:
-    #     xy='0'+xy
-    #     if len(xy.split('d')) == 2:
-    #         xy = xy.split('d')
-    #         x = xy[0]
-    #         y = xy[1]
-    #     elif len(xy.split('D')) == 2:
-    #         xy = xy.split('D')
-    #         x = xy[0]
-    #         y = xy[1]
-    # return x, y, z
+if __name__ == '__main__':
+    throw_validator()
